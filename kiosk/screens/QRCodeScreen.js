61ed7621631d0698808a92eff2e00b134c79a0c0
David Xiong <xiongdsat@gmail.com>
2022-04-17 05:12:47 -0700

    qr code UI

--- kiosk/screens/QRCodeScreen.js
+++ kiosk/screens/QRCodeScreen.js
@@ -7,11 +7,20 @@ import SvgQRCode from 'react-native-qrcode-svg';
 
 export default function ExtraScreen() {
   const { userRfid, logout } = useContext(AuthContext);
+  let logoFromFile = require('../assets/Lockr.png');
   return (
-    <View style={styles.container}>
-      <Text>QR Code Screen!</Text>
-      <SvgQRCode value={userRfid}/>
+    <View style={styles.qrcontainer}>
+       <View style={styles.topBar}><Image
+        style={styles.tinyLogo}
+        source={require('../assets/Lockr.png')}
+      /></View>
+      <Text style={styles.qrcode}>Scan the QR code to link your credit card!</Text>
+      <SvgQRCode value={userRfid} size={200} 
+      />
       <Button title="Log Out" onPress={logout} />
+      <View style={styles.qrcodebottom}></View>
+      <View style={styles.qrcodebottom2}></View>
     </View>
+    
   );
 }
